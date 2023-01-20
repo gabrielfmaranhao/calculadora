393b18bb044c8744a77b9b4de7713061723a1255
Gabriel Maranhao <gabrielmaranhao48@gmail.com>
2023-01-20 03:12:49 -0300

    Feat: deploy na vercel

--- src/utils/validations/index.ts
+++ src/utils/validations/index.ts
@@ -5,7 +5,7 @@ yup.setLocale({
 })
 
 export const validationForm = yup.object().shape({
-    amount: yup.number().required().min(1000, "O mínimo para o valor da venda é 1000").transform((value) => parseFloat(value)),
+    amount: yup.number().required().min(1000, "O mínimo para o valor da venda é 1000").max(100000000, "Numero máximo é 100.000.000").transform((value) => parseFloat(value)),
     installments: yup.number().max(12,"O máximo são de 12 parcelas").min(1,"o mínimo de parcelas é 1").integer("O campo tem que ser um número inteiro").required(),
     mdr: yup.number().required().transform((value) => parseFloat(value))
 })
